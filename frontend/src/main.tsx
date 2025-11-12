2b52dfbac2cdcc578f50f2afe64e596560689d5f
김종현 <jhk9027@gmail.com>
2025-11-12 18:29:49 +0900

    fix: delete comment, app name modified

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,6 @@ import { worker } from '@/shared/mocks/browser'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-// Start mock service worker in development
 if (import.meta.env.DEV) {
   worker.start({
     onUnhandledRequest: 'bypass',
